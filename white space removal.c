60398c52ee346c5f1028f02d9e41374320508324
Intesarul Haque Tomal <74251430+Tomal991@users.noreply.github.com>
2021-06-21 20:56:28 +0600

    initial commit

---
+++ white space removal.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+int main()
+{
+    FILE *file1,*file2;
+    char c;
+    file1=fopen("input00.txt","r");
+    file2=fopen("output00.txt","w");
+    while((c=fgetc(file1))!=EOF)
+    {
+        fputc(c,file2);
+        if(c==' ')
+        {
+
+            while((c=fgetc(file1))==' ')
+            {
+
+            }
+            fputc(c,file2);
+        }
+    }
+    //c=fgetc(file1);
+    //printf("%c",c);
+    fclose(file1);
+    fclose(file2);
+}
